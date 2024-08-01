2da1f45509b70d8c8985bfedf1db8c59127cff30
Raquel Maia <raquelmaia860@gmail.com>
2024-08-01 16:04:39 -0300

    códgio de extração de dados e concatenação

--- src/extract_load.py
+++ src/extract_load.py
@@ -1,10 +1,33 @@
-#import
+
+import yfinance as yf
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+
 
 #import de variaveis de ambiente
+commodities = ['CL=F', 'GC=F', 'SI=F'] #ativos da bolsa de valores (dados de cotacao)
 
-#pegar a cotacao do meus ativos
+def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
+    ticker = yf.Ticker(simbolo)
+    dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
+    dados['simbolo'] = simbolo
+    return dados
 
-# concatenar os meus ativos (1..2..3..) -> (1)
+def buscar_todos_dados_commodities(commodities):
+    todos_dados = []
+    for simbolo in commodities:
+        dados = buscar_dados_commodities(simbolo)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)
+
+
+if __name__ == "__main__":
+    dados_concatenados = buscar_todos_dados_commodities(commodities)
+    #salvar_no_postgres(dados_concatenados, schema='public')
+    print(dados_concatenados)
 
 
 #salvar no banco de dados
